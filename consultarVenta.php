2044cd731ff1d2ab2c7b529e6ccee45cbf2daffd
Sebas <sebas.cay@gmail.com>
2022-05-09 21:43:40 -0500

    Servicios completos para faturas-ventas

--- consultarVenta.php
+++ consultarVenta.php
@@ -5,13 +5,18 @@
         $codventa = $_REQUEST['codventa'];
 
 
-        $cnx = mysqli_connect('localhost','root','bdusuarios');
+        $cnx = mysqli_connect('localhost','root','usuarios')or die("No se pudo conectar con la base de datos");
+        $res = $cnx->query("SELECT * FROM ventas WHERE codventa = '$codventa'");
 
+        $json = array();
+
+        foreach($res as $row) {
+            $json['datos'][] = $row;
+        }
+
+        echo json_encode($json);
+
+    }else{
+        echo "Se requiere un codigo de venta para consultar";
     }
-
-
-
-
-
-
 ?>
